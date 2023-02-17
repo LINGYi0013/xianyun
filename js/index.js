1dba30b41f01798c1cf5485aa65792f87c3737b4
LINGYi0013 <111480080+LINGYi0013@users.noreply.github.com>
2023-02-17 11:13:53 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+function showimg()
+{
+    document.getElementById("floating").style.display="block";
+}
+function hideimg()
+{
+    document.getElementById("floating").style.display="none";
+}
+function showhint()
+{
+    document.getElementById("hint").style.display="block";
+}
+function closehint()
+{
+    document.getElementById("hint").style.display="none";
+}
+function showds()
+{
+    document.getElementById("ds").style.display="block";
+}
+function closeds()
+{
+    document.getElementById("ds").style.display="none";
+}
+function showjd()
+{
+    document.getElementById("jd").style.display="block";
+}
+function closejd()
+{
+    document.getElementById("jd").style.display="none";
+}
+
+function opendoc()
+{
+    window.open("https://www.kdocs.cn/l/cvg8ccOGj5sq", "_blank"); 
+}
+function openquest()
+{
+    window.open("http://jiangnanuniversity.mikecrm.com/bw0ylIm", "_blank"); 
+}
+slider.render({
+    elem: '#slideTest2'
+    ,value: 20 //初始值
+  });
